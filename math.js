284f785a41996b24fe470a29fbf2eaaf2fcea85a
Jerredr <jrn.deraedt@gmail.com>
2026-02-21 14:13:42 +1030

    Add subtraction with feature flag logic and tests

--- math.js
+++ math.js
@@ -2,4 +2,12 @@ function sum(a, b) {
   return a + b;
 }
 
-module.exports = { sum };
+function subtract(a, b, ldClient) {
+  const isEnabled = ldClient.variation('enable-subtraction', false);
+  if (!isEnabled) {
+    throw new Error('Subtraction feature is currently disabled');
+  }
+  return a - b;
+}
+
+module.exports = { sum, subtract };
--- math.test.js
+++ math.test.js
@@ -1,9 +1,31 @@
 const test = require('node:test');
 const assert = require('node:assert');
-const { sum } = require('./math');
+const { sum, subtract } = require('./math');
 
 test('sum function', (t) => {
   assert.strictEqual(sum(1, 2), 3);
   assert.strictEqual(sum(-1, 1), 0);
   assert.strictEqual(sum(0, 0), 0);
+});
+
+test('subtract function with feature flag', async (t) => {
+  // Mock LD Client
+  const mockLdClient = {
+    variation: (flag, defaultVal) => {
+      if (flag === 'enable-subtraction') return t.context.enabled;
+      return defaultVal;
+    }
+  };
+
+  await t.test('works when flag is enabled', (t2) => {
+    t.context = { enabled: true };
+    assert.strictEqual(subtract(5, 3, mockLdClient), 2);
+  });
+
+  await t.test('throws error when flag is disabled', (t2) => {
+    t.context = { enabled: false };
+    assert.throws(() => subtract(5, 3, mockLdClient), {
+      message: 'Subtraction feature is currently disabled'
+    });
+  });
 });
